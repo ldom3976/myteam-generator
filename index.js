0fd7760dda487ff8e425e1a62ef775c20f349541
Lisha Ballard <ldom3976@Lishas-MacBook-Air.local>
2023-02-03 10:59:41 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const inquirer = require("inquirer");
+const fs = require('fs');
+const Manager = require('./lib/manager');
+const Intern = require('./lib/intern');
+const Engineer = require('./lib/engineer');
